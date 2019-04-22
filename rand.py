adb45d39783331b9f774bdbea2f0488b3f708389
DoorThief <DoorThief@users.noreply.github.com>
2019-04-22 12:52:08 -0400

    Update rand.py
    - Make a better argument parser

--- rand.py
+++ rand.py
@@ -1,5 +1,15 @@
 import random as r
-from sys import argv
-for x in range(int(argv[1])):
-	print(r.randint(1,(int(argv[2])-1)))
+import argparse
 
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='A random number set generator.')
+parser.add_argument('-s', '--size', type=int,
+                    help='Required: Specify the amount of random numbers to generate.')
+parser.add_argument('-r', '--range', type=int,
+                    help='Required: Specify how large a random number can be.')
+args = parser.parse_args()
+try:
+	for x in range(args.size):
+		print(r.randint(1,(int(args.range-1))))
+except TypeError:
+	parser.print_help()
